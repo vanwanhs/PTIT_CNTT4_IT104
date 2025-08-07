58d3d8075b11d371b27454c978ee2633d8bc0f94
Hikari <nguyenanh194699@gmail.com>
2025-08-07 13:51:37 +0700

    up

--- PTIT_CNTT4_IT104_SESSION03/PTIT_CNTT4_IT104_SESSION03_bai9.ts
+++ PTIT_CNTT4_IT104_SESSION03/PTIT_CNTT4_IT104_SESSION03_bai9.ts
@@ -1,33 +1,33 @@
-function toNumber2(value: number | string) : number | null {
+function toNumber(value: number | string) : number | null {
   if (typeof value === "number") return value;
   const num = parseFloat(value as string);
   return isNaN(num) ? null : num;
 }
 
 function add(a: number | string, b: number | string): number | string {
-  const numA = toNumber2(a);
-  const numB = toNumber2(b);
+  const numA = toNumber(a);
+  const numB = toNumber(b);
   if (numA === null || numB === null) return "du lieu khong hop le";
   return numA + numB;
 }
 
 function subtract(a: number | string, b: number | string): number | string {
-  const numA = toNumber2(a);
-  const numB = toNumber2(b);
+  const numA = toNumber(a);
+  const numB = toNumber(b);
   if (numA === null || numB === null) return "du lieu khong hop le";
   return numA - numB;
 }
 
 function multiply(a: number | string, b: number | string): number | string {
-  const numA = toNumber2(a);
-  const numB = toNumber2(b);
+  const numA = toNumber(a);
+  const numB = toNumber(b);
   if (numA === null || numB === null) return "Du lieu khong hop le";
   return numA * numB;
 }
 
 function divide(a: number | string, b: number | string): number | string {
-  const numA = toNumber2(a);
-  const numB = toNumber2(b);
+  const numA = toNumber(a);
+  const numB = toNumber(b);
   if (numA === null || numB === null) return "Du lieu khong hop le";
   if (numB === 0) return "Khong the chia cho 0";
   return numA / numB;
