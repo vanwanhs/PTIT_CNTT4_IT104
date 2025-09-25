5298d30ce479f566f27405800e0dfefd9774e45b
Hikari <nguyenanh194699@gmail.com>
2025-09-25 14:04:23 +0700

    up

--- PTIT_CNTT4_IT104_SESSION37/Template_UI_Student_Manager/src/store/slices/studentSlice.ts
+++ PTIT_CNTT4_IT104_SESSION37/Template_UI_Student_Manager/src/store/slices/studentSlice.ts
@@ -1,29 +1,33 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import { API_GETALL_STUDENT } from '../../apis/apis';
-    export const getAllUser = createAsyncThunk("getAllUser",async()=>{
-        try{
-            const response = await axios.get(API_GETALL_STUDENT);
-            return response.data;
-        } catch(error) {
-            console.error(error);
-        }
-    });
-const studentSlice = createSlice({
-    name:"student",
-    initialState:{
-        students:[],
-        loading:false,
-    },
-    reducers:{},
-    extraReducers:(builder)=>{
-        builder
-        .addCase(getAllUser.pending,(state,action)=>{
+import { API_GETALL_STUDENT } from "../../apis/apis";
 
-        })
-        .addCase(getAllUser.fulfilled,(state,action)=>{
-            state.loading = true,
-            state.students = action.payload
-        })
-    }
-})
+export const getAllUser = createAsyncThunk("getAllUser", async () => {
+  try {
+    const response = await axios.get(API_GETALL_STUDENT);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const studentSlice = createSlice({
+  name: "student",
+  initialState: {
+    students: [] as any[],
+    loading: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getAllUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.students = action.payload;
+      });
+  },
+});
+
+export default studentSlice.reducer;
